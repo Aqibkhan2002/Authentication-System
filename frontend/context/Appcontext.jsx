916d1eecb268ae9fb632d10c650073b681c0692b
Aqib Khan <aqib62957@gmail.com>
2025-02-03 19:10:03 +0530

    removed .env from frontend and add favicon

--- frontend/context/Appcontext.jsx
+++ frontend/context/Appcontext.jsx
@@ -1,12 +1,10 @@
-import dotenv from "dotenv"; 
-dotenv.config();
 import { createContext, useState } from "react";
 
 export const AppContext=createContext()
 
 export const ContextProvider=(props)=>{
     
-    const backendUrl=process.env.REACT_APP_BACKEND_URL
+    const backendUrl=import.meta.env.VITE_BACKEND_URL
     const [isloggedIn,setisLoggedIn]=useState(false)
     const [userData,setUserData]=useState(false)
 
